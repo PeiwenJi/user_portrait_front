cb2478c3ce90be0c0cc865719643b88a4ccfb82d
Administrator <2931473049@qq.com>
2021-06-14 16:59:41 +0800

    已实现登录、注册、记住密码

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+//导入axios
+import axios from 'axios'
+//挂载axios
+Vue.prototype.$http = axios
+//设置访问根路径
+axios.defaults.baseURL = "http://localhost:9000"
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
